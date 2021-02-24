2357a0a5c1f02caacd9cc5ce9bb9e52e39da2951
Md. Abdur Rahman <46405817+Riad2019@users.noreply.github.com>
2021-02-25 05:14:33 +0600

    commit

---
+++ src/Components/Reducer/PatientReducer.js
@@ -0,0 +1,23 @@
+export const PatientState ={
+
+    patients: []
+}
+export const PatientReducer =(state,action)=>{
+
+    switch(action.type){
+        case 'ADD_PATIENT' :
+            const newPatients ={
+                id: action.id,
+                name: action.name
+            }
+            const allPatients =[...state.patients ,newPatients]
+            return {patients: allPatients};
+        
+        case 'REMOVE_PATIENT' :
+            const remaining = state.patients.filter(pt=>pt.id !== action.id)
+            const newState = {patients: remaining}
+            return newState;
+         default: 
+         return state;
+    }
+}
